9e457122cf255d6aa2b74f246047abb90a356b32
skevin98 <sawadogokevin98@gmail.com>
2022-03-30 20:28:52 +0000

    Creation du projet

---
+++ app/src/main/java/com/ismagi/hotelreservation/MainActivity.java
@@ -0,0 +1,58 @@
+package com.ismagi.hotelreservation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseUser firebaseUser;
+
+    private static final int SPLASH_TIME_OUT = 2000;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (firebaseUser != null) {
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent menuIntent = new Intent(getApplicationContext(), MenuActivity.class);
+                    getIntent().addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(menuIntent);
+                    finish();
+                }
+            }, SPLASH_TIME_OUT);
+        }
+        else {
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent homeIntent = new Intent(getApplicationContext(), ConnexionActivity.class);
+                    getIntent().addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(homeIntent);
+                    finish();
+                }
+            }, SPLASH_TIME_OUT);
+
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
