a6ae7f8f1023c5315ab4363b6ab50b72480c89f0
Kevin SAW <sawadogokevin98@gmail.com>
2022-04-09 13:25:22 +0000

    +Modification de HomeFragment en ListRoomFragment +Ajout de SingleRoomFragment, SingleReservationFragment +Mise à jour du Nav_graph

--- app/src/main/java/com/ismagi/hotelreservation/MenuActivity.java
+++ app/src/main/java/com/ismagi/hotelreservation/MenuActivity.java
@@ -1,14 +1,25 @@
 package com.ismagi.hotelreservation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
-public class MenuActivity extends AppCompatActivity {
+import com.ismagi.hotelreservation.databinding.ActivityMenuBinding;
 
+public class MenuActivity extends AppCompatActivity {
+    ActivityMenuBinding amd;
+    private NavController navController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+        amd = ActivityMenuBinding.inflate(getLayoutInflater());
+        setContentView(amd.getRoot());
+
+        navController = Navigation.findNavController(this,R.id.fragmentContainerView);
+        NavigationUI.setupWithNavController(amd.BottomNavigationView,navController);
+        NavigationUI.setupActionBarWithNavController(this,navController);
     }
 }
